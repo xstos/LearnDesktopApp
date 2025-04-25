a5cd20ad4f5637ba4cd0424ec339dbc2b9318d4e
Chris C <xstos@users.noreply.github.com>
2025-04-24 20:02:13 -0400

    add javascript repl demo

--- src/AvaloniaApplication1/App.axaml.cs
+++ src/AvaloniaApplication1/App.axaml.cs
@@ -3,15 +3,20 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
-
+using Avalonia.Media;
 using AvaloniaApplication1.Views;
+using Jint;
+using Jint.Native;
+using Jint.Native.Json;
+using Jint.Runtime;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AvaloniaApplication1;
 
 public partial class App : Application
 {
-    public static readonly IServiceProvider? ServiceProvider = BuildDependencyGraph().BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true });
+    public static readonly IServiceProvider? ServiceProvider = BuildDependencyGraph()
+        .BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true });
 
     public static ServiceCollection BuildDependencyGraph()
     {
@@ -31,16 +36,59 @@ public partial class App : Application
 
         object GetContent()
         {
-            new StackPanel().Var(out var sp);
-            sp.Children.Add(new TextBox() {Text = "hello world"});
+            var engine = new Engine(cfg => cfg
+                .AllowClr()
+            );
+            var parsingOptions = new ScriptParsingOptions
+            {
+                Tolerant = true,
+            };
+
+            var serializer = new JsonSerializer(engine);
+            new DockPanel().Var(out var sp);
+            sp.Children.Add(new TextBox() { Text = "hello world" }.Var(out var tb));
+            sp.Children.Add(new TextBox().Var(out var tb2));
+            tb2.TextWrapping = TextWrapping.Wrap;
+            tb.TextWrapping = TextWrapping.Wrap;
+            tb.AcceptsReturn = true;
+            tb.TextChanged += (sender, args) =>
+            {
+                try
+                {
+                    var result = engine.Evaluate(tb.Text, parsingOptions);
+                    JsValue str = result;
+                    if (!result.IsPrimitive() && result is not IJsPrimitive)
+                    {
+                        str = serializer.Serialize(result, JsValue.Undefined, "  ");
+                        if (str == JsValue.Undefined)
+                        {
+                            str = result;
+                        }
+                    }
+                    else if (result.IsString())
+                    {
+                        str = serializer.Serialize(result, JsValue.Undefined, JsValue.Undefined);
+                    }
+
+                    tb2.Text = str + "";
+                }
+                catch (JavaScriptException je)
+                {
+                    tb2.Text = je.Message;
+                }
+                catch (Exception e)
+                {
+                    tb2.Text = e.ToString();
+                }
+            };
             return sp;
         }
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             new MainWindow().Var(out var win);
             win.Content = GetContent();
             desktop.MainWindow = win;
-
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
